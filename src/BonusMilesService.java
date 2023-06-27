6bed112dd25b799afc4d02ba3eb1758eae53ac2b
Max Shvagerus <mshvagerus@gmail.com>
2023-06-28 02:01:17 +0300

    add java doc

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,9 @@
 public class BonusMilesService {
-
+    /**
+     * За каждые 20 рублей, потраченные на билет, начисляется 1 миля. Дробные мили не начисляются.
+     * @param cost цена билета
+     * @return количество бонусных миль
+     */
     public int calculate(int cost) {
         int bonus;
         if (cost >= 20) {
